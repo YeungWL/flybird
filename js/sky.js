3ed79d8757832c403701493a8b330e0bf690c90c
Benjamin <Benjamin>
2017-08-09 20:01:01 +0800

    完成天空对象

---
+++ js/sky.js
@@ -0,0 +1,21 @@
+// 天空的构造函数 --封装
+function Sky(option){
+    this.ctx = option.ctx;
+    this.skyImg = option.skyImg;
+    this.x = option.x;
+    this.y = option.y || 0;
+
+    this.skySpeed = 2;
+    this.imgWidth = this.skyImg.width;
+}
+
+Sky.prototype = {
+    constructor:Sky,
+    drawSky:function(){
+        this.x -= this.skySpeed;
+        if(this.x < -this.imgWidth){
+            this.x += 2* this.imgWidth;
+        }
+        this.ctx.drawImage(this.skyImg,this.x,this.y);
+    }
+}
